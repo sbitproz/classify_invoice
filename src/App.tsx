be17c925e74ce589f604383feeb20a7426429356
Dan Clovis <dan.clovis1@gmail.com>
2020-08-07 11:49:21 +0100

    added Material UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 
 import { Logs } from "./components/Logs";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function App() {
   return (
     <div data-testid="app">
       <Logs />
+      {/* <i className="fas fa-binoculars"></i>
+      <i className="fas fa-arrows-alt-h"></i> */}
+      <FontAwesomeIcon icon={["fas", "binoculars"]} />
     </div>
   );
 }
--- src/components/Logs/Logs.tsx
+++ src/components/Logs/Logs.tsx
@@ -1,4 +1,10 @@
 import React, { useEffect, useCallback } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
 
 // Own
 import { logsService } from "./services/logsService";
@@ -6,14 +12,62 @@ import { logsService } from "./services/logsService";
 // Styles
 import "./Logs.scss";
 
+const useStyles = makeStyles({
+  root: {
+    minWidth: 275,
+  },
+  bullet: {
+    display: "inline-block",
+    margin: "0 2px",
+    transform: "scale(0.8)",
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+});
+
 const Logs: React.SFC = () => {
+  const classes = useStyles();
   const updateLogState = useCallback((response?: any) => {}, []);
 
   useEffect(() => {
     logsService.getLogs().subscribe(updateLogState);
   }, [updateLogState]);
 
-  return <div id="logs"></div>;
+  const bull = <span className={classes.bullet}>•</span>;
+
+  return (
+    <div id="logs">
+      <Card className={classes.root} variant="outlined">
+        <CardContent>
+          <Typography
+            className={classes.title}
+            color="textSecondary"
+            gutterBottom
+          >
+            Word of the Day
+          </Typography>
+          <Typography variant="h5" component="h2">
+            be{bull}nev{bull}o{bull}lent
+          </Typography>
+          <Typography className={classes.pos} color="textSecondary">
+            adjective
+          </Typography>
+          <Typography variant="body2" component="p">
+            well meaning and kindly.
+            <br />
+            {'"a benevolent smile"'}
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small">Learn More</Button>
+        </CardActions>
+      </Card>
+    </div>
+  );
 };
 
 export default Logs;
